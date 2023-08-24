4632120de3e4d6ab9dcff5d33a0187cbb8a429bf
BloodJuice <falcon.with.hat@gmail.com>
2023-08-24 11:35:23 +0300

    Can parse string data

--- reciever.py
+++ reciever.py
@@ -13,22 +13,33 @@ def main():
 
     channel.basic_qos(prefetch_count=1)
     channel.basic_consume(queue='task_queue', on_message_callback=callback)
-    print("dfgdfgdf")
     channel.start_consuming()
 
 
 def callback(ch, method, properties, body):
-    file, nameNeuralMethod = body.decode(), ''
+    file = body.decode()
+    data = dict()
     print(f" [x] Received {file}\n[x] Done")
-    file = file.split()
-    for step in range(len(file)):
-        if file[step] == 'param':
-            nameNeuralMethod = file[step + 2]
-    # if nameNeuralMethod == 'colorizer':
+    print(f'parse:\t{parseInputData(file=file.split())}')
+
     time.sleep(body.count(b'.'))
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
+def parseInputData(file):
+    data = dict()
+    for step in range(len(file)):
+        if file[step] == 'param':
+            data['param'] = file[step + 2]
+            i = 0
+            while file[i] != 'param':
+                if file[i] == 'pic1':
+                    data['picOne'] = file[i + 2]
+                elif file[i] == 'pic2':
+                    data['picTwo'] = file[i + 2]
+                i += 1
+            break
+    return data
 
 
 main()
