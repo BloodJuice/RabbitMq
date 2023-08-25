63477fc92335ecec24de7b92ba72f62bb3bbbd84
BloodJuice <falcon.with.hat@gmail.com>
2023-08-25 15:47:58 +0300

    It works

--- sender.py
+++ sender.py
@@ -3,6 +3,9 @@ import pika
 import sys
 
 def main():
+    # sendHard()
+    sendEasy()
+def sendHard():
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
@@ -23,6 +26,23 @@ def main():
         print(f" [x] Sent {message}")
     connection.close()
 
+def sendEasy():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='task_queue', durable=True)
+
+
+    message = ' '.join(sys.argv[1:]) or "pic1 = 300 pic2 = 200 param = Hello,World!"
+    channel.basic_publish(
+        exchange='',
+        routing_key='task_queue',
+        body=message,
+        properties=pika.BasicProperties(
+            delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+        ))
+    print(f" [x] Sent {message}")
+    connection.close()
 
 
 main()
