1965cb25388b8fada7853e4aebcd8894c6aa710d
BloodJuice <falcon.with.hat@gmail.com>
2023-08-31 14:47:34 +0300

    It recieves json data

--- sender.py
+++ sender.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python
+import json
+
 import pika
 import sys
 
 def main():
-    # sendHard()
-    sendEasy()
+    sendHard()
+    # sendEasy()
 def sendHard():
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
     channel.queue_declare(queue='task_queue', durable=True)
 
-    nameNeuralTest = ['colorizer', 'delete_background', 'upscaler', 'image_to_image', 'text_to_image',
-                      'image_captioning', 'translation', 'inpainting', 'stylization', 'image_fusion']
+    data = {
+        "OrderId": '123e4567-e89b-12d3-a456-426655440000',
+        "params": {"ckpt": "ColorizeArtistic_gen", "steps": 1, "compare": False, "artistic": True, "render_factor": 12, "post_process": True, "clr_saturation_factor": 5, "line_color_limit": 100, "clr_saturate_every_step": True},
+        "enum": 'colorizer',
+        "pic1": 'gththfyhy//dfthtfh5678899dfgfhtfhy656566776'
+    }
 
-    for partOfMessage in nameNeuralTest:
-        message = ' '.join(sys.argv[1:]) or "pic1 = 300 pic2 = 200 param = " + partOfMessage
-        channel.basic_publish(
-            exchange='',
-            routing_key='task_queue',
-            body=message,
-            properties=pika.BasicProperties(
-                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
-            ))
-        print(f" [x] Sent {message}")
+    message = json.dumps(data)
+    channel.basic_publish(
+        exchange='',
+        routing_key='task_queue',
+        body=message,
+        properties=pika.BasicProperties(
+            delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+        ))
+    print(f" [x] Sent {message}")
     connection.close()
 
 def sendEasy():
